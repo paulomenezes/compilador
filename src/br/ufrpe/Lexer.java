db34a43e5dab19de58e4363284be3f69c22ce018
Paulo Menezes <paulomenezes@Mini-de-Paulo.home>
2016-04-21 13:44:21 -0300

    Initial Lexer

---
+++ src/br/ufrpe/Lexer.java
@@ -0,0 +1,64 @@
+package br.ufrpe;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Lexer {
+	private InputStream input;
+	private int nextChar;
+	
+	public Lexer() {
+		this.nextChar = -1;
+	} 
+	
+	public void reset(InputStream in) throws Exception {
+		try {
+			this.input = in;
+			this.nextChar = in.read();		
+		} catch (IOException e) {
+			throw new Exception(e.getMessage());
+		}
+	}
+	
+	private int readByte() throws Exception {
+		int theByte;
+		
+		try {
+			theByte = input.read();
+		} catch (IOException e) {
+			throw new Exception(e.getMessage());
+		}
+		
+		return theByte;
+	}
+	
+	public Token nextToken() throws Exception {
+		TokenType tipo;
+		
+		if (this.nextChar == -1) {
+			return new Token(TokenType.EOF);
+		}
+
+		if (nextChar >= '0' && nextChar <= '9') {
+			tipo = TokenType.NUM_LITERAL;
+		} else if (nextChar == ';') {
+			tipo = TokenType.PT_VIRG;
+		} else if (nextChar == '+') {
+			tipo = TokenType.ADD;
+		} else if (nextChar == '*') {
+			tipo = TokenType.MULT;
+		} else if (nextChar == '(') {
+			tipo = TokenType.ABRE_PAR;
+		} else if (nextChar == ')') {
+			tipo = TokenType.FECHA_PAR;
+		} else {
+			throw new Exception("Caracter inesperado: " + (char)nextChar);
+		}
+		
+		String lexema = "" + (char)nextChar;
+		
+		this.nextChar = this.readByte();
+
+		return new Token(tipo, lexema);
+	}
+}
---
+++ src/br/ufrpe/TokenType.java
@@ -0,0 +1,11 @@
+package br.ufrpe;
+
+public enum TokenType {
+	NUM_LITERAL,
+	PT_VIRG,
+	ADD,
+	MULT,
+	ABRE_PAR,
+	FECHA_PAR,
+	EOF
+}
